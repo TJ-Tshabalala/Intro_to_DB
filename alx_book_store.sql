ee6b0990ee4e8d3bff91126f0719f2e709e2a934
fakeLemonz <156143921+TJ-Tshabalala@users.noreply.github.com>
2025-02-10 08:41:25 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,23 +17,18 @@ LOCK TABLES Authors WRITE;
 /*!40000 ALTER TABLE `Authors` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Authors` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `Books`
---
-
-DROP TABLE IF EXISTS `Books`;
-CREATE TABLE `Books` (
-  `book_id` int NOT NULL,
-  `title` varchar(130) NOT NULL,
-  `author_id` int DEFAULT NULL,
-  `price` double NOT NULL,
-  `publication_date` date DEFAULT NULL,
-  PRIMARY KEY (`book_id`),
-  KEY `author_id` (`author_id`),
-  CONSTRAINT `Books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `Authors` (`author_id`)
+DROP TABLE IF EXISTS `Order_Details`;
+CREATE TABLE `Order_Details` (
+    order_detailid INT NOT NULL,
+    order_id INT DEFAULT NULL,
+    book_id INT DEFAULT NULL,
+    quantity DOUBLE DEFAULT NULL,
+    PRIMARY KEY (order_detailid),
+    KEY order_id (order_id),
+    KEY book_id (book_id),
+    CONSTRAINT FK_OrderDetailOrder FOREIGN KEY (order_id) REFERENCES Orders(order_id),  -- No backticks here
+    CONSTRAINT FK_OrderDetailBook FOREIGN KEY (book_id) REFERENCES Books(book_id)   -- No backticks here
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 --
 -- Dumping data for table `Books`
