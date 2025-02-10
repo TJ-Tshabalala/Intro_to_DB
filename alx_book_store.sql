424e50dde6e7ddcce952ffafdaa5ecae0fbca72c
fakeLemonz <156143921+TJ-Tshabalala@users.noreply.github.com>
2025-02-10 08:31:42 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -100,7 +100,7 @@ CREATE TABLE Orders (
   order_id INT NOT NULL,
   customer_id INT DEFAULT NULL,
   order_date DATE DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
+  PRIMARY KEY (order_id),
   KEY customer_id (customer_id),
   CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
